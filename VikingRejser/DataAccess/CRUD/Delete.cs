c01cb49eb74df322279219682741e6d0bb5631e7
matt2256 <mads@mmm-m.dk>
2020-06-17 13:42:36 +0200

    Finished with CRUD for clients

--- VikingRejser/DataAccess/CRUD/Delete.cs
+++ VikingRejser/DataAccess/CRUD/Delete.cs
@@ -3,10 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Entities;
 
 namespace DataAccess.CRUD
 {
-    class Delete
+    public class Delete
     {
+        /// <summary>
+        /// Represent a method for deleting client data
+        /// </summary>
+        /// <param name="_Id">Represent the Id (int) of the client</param>
+        public void DeleteClient(int _Id)
+        {
+            //Represent a object of VikingRejser called context
+            VikingRejserEntities context = new VikingRejserEntities();
+
+            Kunder client = context.Kunders.First(i => i.Id == _Id);
+
+            context.Kunders.Remove(client);
+        }
     }
 }
--- VikingRejser/DataAccess/CRUD/Edit.cs
+++ VikingRejser/DataAccess/CRUD/Edit.cs
@@ -3,10 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Entities;
 
 namespace DataAccess.CRUD
 {
-    class Edit
+    public class Edit
     {
+        /// <summary>
+        /// Represent a method for editing client data
+        /// </summary>
+        /// <param name="_Id">Represent the id (int) of the client</param>
+        /// <param name="_Name">Represnet the name (string) of the client</param>
+        /// <param name="_Address">Represnet the address (string) of the client</param>
+        /// <param name="_Cellphone">Represent the cellphone number (string) of the client</param>
+        /// <param name="_TravelArragement">Represent the TravelArragement (string) of the client</param>
+        public void EditClient(int _Id, string _Name, string _Address, string _Cellphone, string _TravelArragement)
+        {
+            //Represent a object of VikingRejser called context
+            VikingRejserEntities context = new VikingRejserEntities();
+
+            Kunder client = context.Kunders.First(i => i.Id == _Id);
+
+            //Represent the name (string) of the client
+            client.Navn = _Name;
+
+            //Represnet the address (string) of the client
+            client.Adresse = _Address;
+
+            //Represent the cellphone number (string) of the client
+            client.Telefon = _Cellphone;
+
+            //Represent the TravelArragement (string) of the client
+            client.RejseArrangementer = _TravelArragement;
+
+            context.SaveChanges();
+        } 
     }
 }
