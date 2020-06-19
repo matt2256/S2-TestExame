add6af162e29fc2dad0a9a1090ec9ee3bcc76c79
matt2256 <mads@mmm-m.dk>
2020-06-19 12:06:22 +0200

    Added public classes to api dll
    Added a public method to control the api call
    
    Added a class to hold and controll all data being recived from OpenWeaterhMap - Not finished with summary comments

---
+++ VikingRejser/API/API.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Net;
+namespace API
+{
+    class API
+    {
+        /// <summary>
+        /// Represent a method that uses a api for getting data about the weather
+        /// </summary>
+        /// <param name="_cityName">Represent the input (string) for the city</param>
+        public void GetWeatherData(string _cityName, int _key)
+        {
+            using(WebClient web = new WebClient())
+            {
+                string url = string.Format($"api.openweathermap.org/data/2.5/weather?q={_cityName}&appid={_key}");
+
+                var json = web.DownloadString(url);
+
+                var result = JsonConvert.DeserializeObject<WeatherInformation.root>(json);
+
+                WeatherInformation.root output = result;
+
+
+            }
+        }
+    }
+}
---
+++ VikingRejser/API/WeatherInformation.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API
+{
+    public class WeatherInformation
+    {
+        /// <summary>
+        /// Represent a public class, used to access coord data from OpemWeatherMap api
+        /// </summary>
+        public class coord
+        {
+            //Represent the longitude 
+            public double lon { get; set; }
+
+            //Represent the latitude 
+            public double lat { get; set; }
+        }
+
+        /// <summary>
+        /// Represent a public class, used to access weather data from OpenWeatherMap api
+        /// </summary>
+        public class weather
+        {
+            //Represent what type the weather is etc. cloods
+            public string main { get; set; }
+        }
+
+        /// <summary>
+        /// Represent a public class, used to access main data form OpenWeatherMap api
+        /// </summary>
+        public class main
+        {
+            //Represent what the temperature is 
+            public double temp { get; set; }
+        }
+
+        public class wind
+        {
+            public int speed { get; set; }
+        }
+
+        public class sys
+        {
+            public string country { get; set; }
+        }
+
+        public class root
+        {
+            public string name { get; set; }
+            public sys sys { get; set; }
+            public double dt { get; set; }
+            public main main { get; set; }
+            public List<weather> Weathers { get; set; }
+            public coord coord { get; set; }
+        }
+    }
+}
