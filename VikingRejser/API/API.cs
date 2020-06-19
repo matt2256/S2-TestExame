529c8f5a65f472111c8f2a1b963e502db9cad6c3
mads nielsen kjerumgaard <mads@mmm-m.dk>
2020-06-19 18:30:55 +0200

    Added comments to API class

--- VikingRejser/API/API.cs
+++ VikingRejser/API/API.cs
@@ -10,17 +10,24 @@ namespace API
     class API
     {
         /// <summary>
-        /// Represent a method that uses a api for getting data about the weather
+        /// Represent a method for retriving weather data form OpenWeatherMap´s api
         /// </summary>
-        /// <param name="_cityName">Represent the input (string) for the city</param>
-        public void GetWeatherData(string _cityName, int _key, object _weatherType, object _temp, object _windSpeed, object _countryName )
+        /// <param name="_cityName">Represent the input name (string) of the city</param>
+        /// <param name="_key">Represent the API key (string)used for authentication on OpenWeatherMap.com</param>
+        /// <param name="_weatherType">Represent the parameter (object) used to parse the weather type </param>
+        /// <param name="_temp">Represent the parameter (object) used to parse the temperature </param>
+        /// <param name="_windSpeed">Represent the parameter (object) used to parse the windspeed</param>
+        /// <param name="_countryName">Represent the parameter (object) used to parse the county name </param>
+        public void GetWeatherData(string _cityName, string _key, object _weatherType, object _temp, object _windSpeed, object _countryName )
         {
             using(WebClient web = new WebClient())
             {
+                //Represent url being formatted to a string 
                 string url = string.Format($"api.openweathermap.org/data/2.5/weather?q={_cityName}&appid={_key}");
 
+                //Represent a json variable set to download a web string called url 
                 var json = web.DownloadString(url);
-
+                
                 var result = JsonConvert.DeserializeObject<WeatherInformation.root>(json);
 
                 WeatherInformation.root output = result;
